2e26847b0006d6bc9fb4c398f9911971c06b9e11
Abad Vera <abad_vera@me.com>
2026-02-16 10:59:14 -0800

    Fix mirrored text

--- src/include/data/data.hpp
+++ src/include/data/data.hpp
@@ -247,7 +247,7 @@ public:
     ){
         for (size_t xi = 0; xi < sprite.width; ++xi) {
             for (size_t yi = 0; yi < sprite.height; ++yi) {
-                size_t sy = sprite.height - 1 + y - yi;
+                size_t sy = y + yi;          // ← was: sprite.height - 1 + y - yi
                 size_t sx = x + xi;
                 if (sprite.data[yi * sprite.width + xi]
                     && sy < height && sx < width) {
